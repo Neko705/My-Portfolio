8343801fbca9659c82ec413f05951807662765c6
Neko705 <129487693+Neko705@users.noreply.github.com>
2023-08-31 14:16:41 +0530

    day 2

---
+++ portfolio/src/components/Sidbar/index.js
@@ -0,0 +1,5 @@
+import './index.scss'
+
+const Sidebar = () => <>Hello from side bar</>
+
+export default Sidebar;
---
+++ portfolio/src/components/layout/index.js
@@ -0,0 +1,11 @@
+import Sidebar from '../Sidbar';
+import './index.scss';
+
+const Layout = () => {
+    return (
+    <>
+    <Sidebar />
+    </>
+    );
+}
+export default Layout;
